f86211e98e808a3f82b885fb0348d0b2b3f0905f
hanlee <me@hanlee.co>
2021-01-21 16:41:46 +0900

    chore: update deps

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from 'vite-plugin-pages/client'
-
 import App from './App.vue'
 import './index.css'
+
+console.log(routes)
 
 const router = createRouter({
   history: createWebHistory(),
@@ -15,5 +16,3 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
-
-console.log(router)
