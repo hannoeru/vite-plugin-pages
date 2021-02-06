a176a1c69971f2ad12455e3e8238d32eaec7bb5e
John Campion <john@brightshore.com>
2021-02-06 11:56:55 -0500

    Remove inline require
    Not needed as we're targeting vue3

--- src/parseSfc.ts
+++ src/parseSfc.ts
@@ -1,4 +1,4 @@
-import type { parse as _parserV3 } from '@vue/compiler-sfc';
+import { parse } from '@vue/compiler-sfc'
 
 export interface CustomBlock {
   type: string
@@ -11,11 +11,11 @@ export interface ParseResult {
 
 export function parseSFC(code: string): ParseResult {
   try {
-    const parserV3: typeof _parserV3 = require('@vue/compiler-sfc').parse
-    return parserV3(code, {
+    return parse(code, {
       pad: 'space',
     }).descriptor
-  } catch {
+  }
+  catch {
     throw new Error('[vue-route-generator] Vue3\'s "@vue/compiler-sfc" is required.')
   }
 }
