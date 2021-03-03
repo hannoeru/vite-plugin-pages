52dce7ce0530a35690701dcce2e1e538e2aae96c
hanlee <me@hanlee.co>
2021-03-04 05:01:21 +0900

    test: parser

---
+++ test/parse.spec.ts
@@ -0,0 +1,27 @@
+import fs from 'fs'
+import { resolve } from 'path'
+import { parseSFC, tryParseCustomBlock } from '../src/parseSfc'
+import { resolveOptions } from '../src/index'
+
+const options = resolveOptions({})
+const path = resolve('./test/pages/user/[id].vue')
+const VueFile = fs.readFileSync(path, 'utf-8')
+
+const expectCustomBlock = {
+  meta: {
+    requiresAuth: false,
+  },
+  name: 'name-override',
+}
+
+describe('Parser', () => {
+  const parsed = parseSFC(VueFile)
+  const customBlock = parsed.customBlocks.find(b => b.type === 'route')!
+  test('SFC', () => {
+    expect(parsed).toMatchSnapshot()
+  })
+  test('custom block', () => {
+    const parsedCustomBlock = tryParseCustomBlock(customBlock, path, options)
+    expect(parsedCustomBlock).toStrictEqual(expectCustomBlock)
+  })
+})
