4a65d2d346074b46a5d5714b7ae48a3ebb4e5d67
hanlee <me@hanlee.co>
2021-03-07 13:56:50 +0900

    test: resolve options

---
+++ test/options.spec.ts
@@ -0,0 +1,20 @@
+import { resolveOptions } from '../src/index'
+
+const resolvedOptions = {
+  routeBlockLang: 'json5',
+  root: process.cwd(),
+  pagesDir: ['src/pages'],
+  pagesDirOptions: [{ dir: 'src/pages', baseRoute: '' }],
+  extensions: ['vue', 'js'],
+  importMode: 'async',
+  exclude: [],
+  syncIndex: true,
+  replaceSquareBrackets: false,
+}
+
+describe('Options', () => {
+  test('resolve', () => {
+    const options = resolveOptions({})
+    expect(options).toStrictEqual(resolvedOptions)
+  })
+})
