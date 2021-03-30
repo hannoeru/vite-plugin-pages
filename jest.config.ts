af41b2833e7c63040a6851e7f1c4b847e0259664
hanlee <me@hanlee.co>
2021-03-30 23:12:03 +0900

    chore: change jest config to ts file, update deps

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
-module.exports = {
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
   testEnvironment: 'node',
   testMatch: ['**/test/**/*.spec.[jt]s?(x)'],
   transform: {
     '^.+\\.tsx?$': 'jest-esbuild',
   },
 }
+
+export default config
