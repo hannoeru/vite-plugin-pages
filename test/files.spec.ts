2e513bbff5ea21bed07ed9577b371adbfc7428e5
hanlee <me@hanlee.co>
2021-05-19 02:39:05 +0900

    fix: fast-glob patterns windows  path normalize

--- test/files.spec.ts
+++ test/files.spec.ts
@@ -1,10 +1,11 @@
-import { resolve } from 'path'
+import { join } from 'path'
+import { slash } from '../src/utils'
 import { resolveOptions } from '../src/options'
 import { getPageFiles, getPageDirs } from '../src/files'
 
 const options = resolveOptions({})
-const testPagesDir = resolve('test/assets/pages')
-const testDeepPagesDir = resolve('test/assets/deep-pages')
+const testPagesDir = 'test/assets/pages'
+const testDeepPagesDir = 'test/assets/deep-pages'
 
 describe('Get files', () => {
   test('Pages file', async() => {
@@ -16,19 +17,19 @@ describe('Get files', () => {
 describe('Get page dirs', () => {
   test('With glob', async() => {
     const pageDirOptions = {
-      dir: resolve(testDeepPagesDir, '**', 'pages'),
-      baseRoute: '/',
+      dir: slash(join(testDeepPagesDir, '**', 'pages')),
+      baseRoute: '',
     }
     const dirs = getPageDirs(pageDirOptions, options.root, options.exclude)
     const sortedDirs = dirs.sort()
     expect(sortedDirs).toHaveLength(2)
-    expect(sortedDirs[0].baseRoute).toBe('/')
+    expect(sortedDirs[0].baseRoute).toBe('')
     expect(sortedDirs[0].dir).toBe(
-      resolve(testDeepPagesDir, 'bar', 'pages'),
+      slash(join(testDeepPagesDir, 'bar', 'pages')),
     )
-    expect(sortedDirs[1].baseRoute).toBe('/')
+    expect(sortedDirs[1].baseRoute).toBe('')
     expect(sortedDirs[1].dir).toBe(
-      resolve(testDeepPagesDir, 'foo', 'pages'),
+      slash(join(testDeepPagesDir, 'foo', 'pages')),
     )
   })
 })
