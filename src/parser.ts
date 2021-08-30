a9fa8d586a73b003edb0602dad093d5b3dd43b7c
hanlee <me@hanlee.co>
2021-08-30 22:29:02 +0900

    chore: update deps

--- src/parser.ts
+++ src/parser.ts
@@ -21,19 +21,19 @@ export function parseCustomBlock(block: SFCBlock, filePath: string, options: Res
   if (lang === 'json5') {
     try {
       return JSON5.parse(block.content)
-    } catch (err) {
+    } catch (err: any) {
       throw new Error(`Invalid JSON5 format of <${block.type}> content in ${filePath}\n${err.message}`)
     }
   } else if (lang === 'json') {
     try {
       return JSON.parse(block.content)
-    } catch (err) {
+    } catch (err: any) {
       throw new Error(`Invalid JSON format of <${block.type}> content in ${filePath}\n${err.message}`)
     }
   } else if (lang === 'yaml' || lang === 'yml') {
     try {
       return YAML.parse(block.content)
-    } catch (err) {
+    } catch (err: any) {
       throw new Error(`Invalid YAML format of <${block.type}> content in ${filePath}\n${err.message}`)
     }
   }
