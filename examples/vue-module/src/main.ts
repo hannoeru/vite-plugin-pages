c37a07629c72d03c1038b7afd8cc678421326cb0
hanlee <me@hanlee.co>
2021-12-18 16:32:52 +0900

    chore: fix import order

--- examples/vue-module/src/main.ts
+++ examples/vue-module/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import routes from '~pages'
 import App from './App.vue'
+import routes from '~pages'
 import './index.css'
 
 // eslint-disable-next-line no-console
