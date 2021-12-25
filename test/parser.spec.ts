9048e245f7211bd27cd8f87e6c37f5f7bdea83d1
hanlee <me@hanlee.co>
2021-12-26 07:33:11 +0900

    chore: parser test

--- test/parser.spec.ts
+++ test/parser.spec.ts
@@ -1,18 +1,14 @@
-import fs from 'fs'
 import { resolve } from 'path'
 import { describe, expect, test } from 'vitest'
-import { parseCustomBlock, parseSFC } from '../src/customBlock'
+import { getRouteBlock } from '../src/customBlock'
 import { resolveOptions } from '../src/options'
 
 const options = resolveOptions({})
 const path = resolve('./examples/vue/src/pages/blog/[id].vue')
-const VueFile = fs.readFileSync(path, 'utf-8')
 
 describe('Parser', () => {
   test('custom block', async() => {
-    const parsed = await parseSFC(VueFile)
-    const customBlock = parsed.customBlocks.find(b => b.type === 'route')!
-    const parsedCustomBlock = parseCustomBlock(customBlock, path, options)
-    expect(parsedCustomBlock).toMatchSnapshot()
+    const routeBlock = await getRouteBlock(path, options)
+    expect(routeBlock).toMatchSnapshot()
   })
 })
