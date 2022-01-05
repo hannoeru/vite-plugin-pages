d009eba3b252d2407a69ea5d20cc08a89ea51977
hanlee <me@hanlee.co>
2022-01-06 01:03:14 +0900

    chore: support beta tag in release

---
+++ scripts/publish.ts
@@ -0,0 +1,9 @@
+import { execSync } from 'child_process'
+import { version } from '../package.json'
+
+let command = 'pnpm publish --access public --no-git-checks'
+
+if (version.includes('beta'))
+  command += ' --tag beta'
+
+execSync(command, { stdio: 'inherit' })
