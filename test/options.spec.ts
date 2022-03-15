ccd9fa0ef313eeb87902d4a7be1914e1db6b39fe
hanlee <me@hanlee.co>
2022-03-16 02:02:23 +0900

    chore: solid options resolve test

--- test/options.spec.ts
+++ test/options.spec.ts
@@ -1,20 +1,33 @@
 import { describe, expect, test } from 'vitest'
 import { resolveOptions } from '../src/options'
 
-describe('Options', () => {
-  test('resolve', () => {
+describe('Options resolve', () => {
+  test('vue', () => {
     const options = resolveOptions({
       dirs: 'examples/vue/src/pages',
     })
-    const reactOptions = resolveOptions({
+    expect(options).toMatchSnapshot({
+      root: expect.any(String),
+    })
+  })
+
+  test('react', () => {
+    const options = resolveOptions({
       dirs: 'examples/react/src/pages',
       resolver: 'react',
     })
     expect(options).toMatchSnapshot({
       root: expect.any(String),
-    }, 'vue')
-    expect(reactOptions).toMatchSnapshot({
+    })
+  })
+
+  test('solid', () => {
+    const options = resolveOptions({
+      dirs: 'examples/solid/src/pages',
+      resolver: 'solid',
+    })
+    expect(options).toMatchSnapshot({
       root: expect.any(String),
-    }, 'react')
+    })
   })
 })
