7f551159bbd32399ece22c7fa078a08986f9b44e
hanlee <me@hanlee.co>
2022-06-14 00:24:49 +0900

    chore: fix lint

--- examples/vue/src/pages/jsx.jsx
+++ examples/vue/src/pages/jsx.jsx
@@ -13,6 +13,7 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'TestJSX',
   setup() {
+    // eslint-disable-next-line react/display-name
     return () => <div>TestJSX</div>
   },
 })
