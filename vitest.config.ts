77230843cb9984448035c170fb9b9d08f61f282c
hanlee <me@hanlee.co>
2022-06-25 13:35:32 +0900

    chore: exclude react e2e test for now

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
+    include: ['test/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    exclude: ['test/e2e/react.test.ts'],
   },
 })
