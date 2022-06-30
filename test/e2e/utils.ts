5f5485dd7f248cbcee21a058310cf71fd37be99b
hanlee <me@hanlee.co>
2022-06-30 12:25:57 +0900

    chore: Revert "chore: remove watcher limit on e2e test"
    This reverts commit a60fef33935e809bf535c3a53c9a6098a42be641.

--- test/e2e/utils.ts
+++ test/e2e/utils.ts
@@ -4,6 +4,12 @@ export const getViteConfig = (root: string): UserConfig => ({
   root,
   logLevel: 'silent',
   server: {
+    watch: {
+      // During tests we edit the files too fast and sometimes chokidar
+      // misses change events, so enforce polling for consistency
+      usePolling: true,
+      interval: 100,
+    },
     host: true,
   },
   build: {
