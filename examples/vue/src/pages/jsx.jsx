5aa84e63bdb4b5adc4dddf47bcfd6edeeadff69d
hanlee <me@hanlee.co>
2022-08-27 17:05:20 +0900

    chore: fix lint error

--- examples/vue/src/pages/jsx.jsx
+++ examples/vue/src/pages/jsx.jsx
@@ -13,7 +13,6 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'TestJSX',
   setup() {
-    // eslint-disable-next-line react/display-name
     return () => <div>TestJSX</div>
   },
 })
--- test/e2e/solid.test.ts
+++ test/e2e/solid.test.ts
@@ -1,4 +1,3 @@
-/* eslint-disable jest/no-standalone-expect */
 import { resolve } from 'path'
 import { copyFile, rm } from 'fs/promises'
 import { existsSync } from 'fs'
