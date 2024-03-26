4e07846e253bf7753ca20023d3e583c8b23b517a
hanlee <me@hanlee.co>
2024-03-26 23:55:21 +0900

    chore: separate e2e test

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    include: ['test/**/*.spec.ts'],
+    include: ['e2e/**/*.test.ts'],
     globals: true,
   },
 })
