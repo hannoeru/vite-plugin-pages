4dbd18cec8647409c3ce028cd51a3a1f0366edac
hanlee <me@hanlee.co>
2025-02-19 18:43:52 +0900

    chore: update deps

--- examples/solid/src/pages/[sensor]/current.tsx
+++ examples/solid/src/pages/[sensor]/current.tsx
@@ -1,7 +1,7 @@
 import { useParams } from '@solidjs/router'
 
 export default function Current() {
-  const { sensor } = useParams < { sensor: string }>()
+  const { sensor } = useParams <{ sensor: string }>()
   return (
     <p>
       /
